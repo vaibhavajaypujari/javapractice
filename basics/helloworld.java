b5d21679b116a5b197579237581f92d0ceefb207
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-03 23:25:02 +0530

    Add HelloWorld Java program

---
+++ basics/helloworld.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class helloworld {
+	public static void main(String[] args) {
+		System.out.println("hello everyone");
+		
+		System.out.println("________________/n");
+				
+		System.out.println("welcome to my practice phase");
+		
+		System.out.println("________________/n");
+		
+		System.out.println("lets see my codes");
+		
+		System.out.println("________________/n");
+		
+		System.out.println("*-*-*Thank you*-*-*");
+	}
+} 
