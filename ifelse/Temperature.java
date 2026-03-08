2b3469ca21fdf2bfd690e40d6afd66390c93057f
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-08 17:14:29 +0530

    if else Temperature is hi/lo code

---
+++ ifelse/Temperature.java
@@ -0,0 +1,42 @@
+package ifelse;
+
+public class Temperature {
+public static void main(String[] args) {
+	int temp =32;
+	
+	if(temp>30) {
+		System.out.println("Temperature is High");
+	}else {
+		System.out.println("Temperature is Normal");
+	}
+}
+}
+
+//practice
+
+
+//public class Temperature{
+//	public static void main(String[] args) {
+//		int temp=45;
+//		
+//		if(temp>30) {
+//			System.out.println("this Temperature is High");
+//		}else {
+//		
+//		System.out.println("this Temperature is Normal");
+//	}
+//}
+//}
+
+
+//public class Temperature{
+//	public static void main(String[] args) {
+//		int temp=15;
+//		
+//		if(temp>30) {
+//			System.out.println("Temperature is High");
+//		}else {
+//			System.out.println("Temperatur is Normal");
+//		}
+//	}
+//}
