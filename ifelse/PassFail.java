fafb0e1150e51cfbda99b6ef753a0ad60e0ab99d
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-08 16:12:44 +0530

    added a pass or fail

---
+++ ifelse/PassFail.java
@@ -0,0 +1,52 @@
+package ifelse;
+
+public class PassFail { 
+	public static void main(String[] args) {
+		int marks=45;
+		
+			if(marks>=35) {
+				System.out.println("You Are PASS");
+			} else {
+				System.out.println("You Are FAIL");
+		}
+	}
+}
+
+//practice 3/4 times 
+
+//public class PassFail{
+//	public static void main (String[] args) {
+//		int marks = 35;
+//		if(marks>=35) {
+//			System.out.println("pass");
+//		}else {
+//			System.out.println("fail");
+//		}
+//	}
+//}
+
+//
+//public class PassFail{
+//	public static void main(String[] args) {
+//		int marks=31;
+//		
+//		if(marks>=35) {
+//			System.out.println("u r pass");
+//			}else {
+//				System.out.println("u r fail");
+//				}
+//			}
+//	}
+
+
+//
+//public class PassFail{
+//	public static void main(String[] args) {
+//		int marks=25;
+//		if(marks>=35) {
+//			System.out.println("you are Passed");
+//		}else {
+//			System.out.println("you are Failed");
+//		}
+//	}
+//}
