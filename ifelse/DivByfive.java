b87ab715d280e053c65a51e01500200d3eae50a4
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-08 17:11:26 +0530

    if else Divide By Five code

---
+++ ifelse/DivByfive.java
@@ -0,0 +1,42 @@
+package ifelse;
+
+public class DivByfive {
+public static void main(String[] args) {
+	int num=15;
+	
+	if(num%5==0) {
+		System.out.println("Divided by 5");
+	}else {
+		System.out.println("Not Divide by 5");
+	}
+}
+}
+
+
+//practice
+
+//public class DivByfive{
+//	public static void main(String[] args) {
+//		
+//		int num=47;
+//		
+//		if(num%5==0) {
+//			System.out.println("divded by 5");
+//		}else {
+//			System.out.println("not divded by 5");
+//		}
+//	}
+//}
+
+
+//public class DivByfive{
+//	public static void main(String[] args) {
+//		int num=0;
+//		
+//		if(num%5==0) {
+//			System.out.println("divided by 5");
+//		}else {
+//			System.err.println("not divided by 5");
+//		}
+//	}
+//}
