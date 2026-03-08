f62519bc4da38ead8041601624b9a8cf8297c8e5
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-08 17:09:03 +0530

    Added a checked zero or not

---
+++ ifelse/CheckZero.java
@@ -0,0 +1,41 @@
+package ifelse;
+
+public class CheckZero {
+	public static void main(String[] args) {
+		int num=0;
+		if(num==0){
+			System.out.println("this number is ZERO");
+		
+	}else {
+		System.out.println("this number is not ZERO");
+		}
+
+	}
+}
+
+
+
+//prcatice 1/2 times
+
+//public class CheckZero{
+//	public static void main( String[] args) {
+//		int num=12;
+//		if(num==0) {
+//			System.out.println("this number is Zero");
+//		}else {
+//			System.out.println("this number is not Zero");
+//		}
+//	}
+//}
+
+
+//public class CheckZero{
+//	public static void main(String[] args) {
+//		int num = 45;
+//		if(num==0) {
+//			System.out.println("its num is ZERO");
+//		}else {
+//			System.out.println("its num is not Zero");
+//		}
+//	}
+//}
