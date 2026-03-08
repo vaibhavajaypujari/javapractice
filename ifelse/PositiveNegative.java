6989b832105867747a8e0a55696334bfb3c3192e
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-08 15:43:41 +0530

    Added a Positive-Negative number

---
+++ ifelse/PositiveNegative.java
@@ -0,0 +1,64 @@
+package ifelse;
+
+//public class PositiveNegative {
+//	public static void main(String[] args) {
+//		int num = 5;
+//		
+//		if(num>0) {
+//			System.out.println("this number is Positive");
+//		}else {
+//			System.out.println("this number is Negative");			
+//		}
+//	}
+//
+//}
+
+//practice 3/4 times
+
+
+//public class PositiveNegative{
+//	public static void main(String [] args) {
+//		int num= -45;
+//		if(num>0) {
+//			System.out.println("positive");
+//		}else {
+//			System.out.println("negative");
+//		}
+//	}	
+//}
+
+
+//public class PositiveNegative{
+//	public static void main(String [] args) {
+//		int num=45;
+//		if(num>0) {
+//			System.err.println("this is positive");
+//		}else {
+//			System.out.println("this is negative");
+//		}
+//	}
+//}
+
+
+//public class PositiveNegative{
+//	public static void main(String[] args) {
+//		int num=01;
+//		if(num>0) {
+//			System.out.println("positive number");
+//		}else {
+//			System.out.println("negative number");
+//		}
+//	}
+//}
+
+
+public class PositiveNegative{
+	public static void main(String[] args) {
+		int num=-01;
+		if(num>0) {
+			System.out.println("its positive number");
+		}else {
+			System.out.println("its negative number");
+		}
+	}
+}
