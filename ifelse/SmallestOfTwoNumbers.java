568a2db9d6df373a6dfd9980e39ae8ca85288243
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-08 17:13:06 +0530

    if else smallest number in two number code

---
+++ ifelse/SmallestOfTwoNumbers.java
@@ -0,0 +1,43 @@
+package ifelse;
+
+public class SmallestOfTwoNumbers {
+public static void main(String[] args) {
+	int a=45;
+	int b=43;
+	
+	if(a<b) {
+		System.out.println("A is Smallest");
+	}else {
+		System.out.println("B is Smallest");
+	}
+}
+}
+
+
+//practice 2/3 times
+
+//public class SmallestOfTwoNumbers{
+//	public static void main(String[] args) {
+//		int a=45;
+//		int b=50;
+//		if(a<b) {
+//			System.out.println("A is small");
+//		}else {
+//			System.out.println("b is small");
+//		}
+//	}
+//}
+
+
+//public class SmallestOfTwoNumbers{
+//	public static void main(String[] args) {
+//		int a=55;
+//		int b=5;
+//		
+//		if(a<b) {
+//			System.out.println("a is small");
+//		}else {
+//			System.out.println("b is small");
+//		}
+//	}
+//}
