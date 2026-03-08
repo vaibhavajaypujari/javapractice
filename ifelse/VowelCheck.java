a7ba1bc4b385640b463f63bb3d6db3accc2a0a0b
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-08 17:16:14 +0530

    if else vowel or Consonant code

---
+++ ifelse/VowelCheck.java
@@ -0,0 +1,42 @@
+package ifelse;
+
+public class VowelCheck {
+	
+	public static void main(String[] args) {
+		char ch = 'a';
+	
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+			System.out.println("this is Vowel");
+		}else {
+			System.out.println("this is Consonant");
+		}
+	}
+}
+
+//practice
+
+
+//public class VowelCheck{
+//	public static void main(String[] args) {
+//		char ch ='t';
+//		
+//		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+//			System.out.println("Vowel");
+//		}else {
+//			System.out.println("Consonant");
+//		}
+//	}
+//}
+
+
+//public class VowelCheck{
+//	public static void main(String[] args) {
+//		char ch= 'g';
+//		
+//		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+//			System.out.println("it is VOWEL");
+//		}else {
+//			System.out.println("it is CONSONANT");
+//		}
+//	}
+//}
