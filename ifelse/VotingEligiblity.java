42ee26c2ca6b9253af5769162687f3e5b6ca1c8c
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-08 15:47:05 +0530

    added a Voting Eligiblity

---
+++ ifelse/VotingEligiblity.java
@@ -0,0 +1,69 @@
+package ifelse;
+
+public class VotingEligiblity {
+public static void main(String[] args) {
+	int age=10;
+	
+	if(age>=18) {
+		System.out.println("you are Eligibile for Voting");
+	}else {
+		System.out.println("you are not Eligible for Voting");
+	}
+}
+}
+
+
+
+//practice 3/4 times 
+
+//
+//public class VotingEligiblity {
+//	public static void main(String[] args) {
+//		int age =15;
+//		
+//		if(age>=18) {
+//			System.out.println("you are eligible");
+//		}else {
+//			System.out.println("you are not eligible");
+//		}
+//	}
+//}
+
+
+//public class VotingEligiblity {
+//	public static void main(String[] args) {
+//		int age=18;
+//		
+//		if(age>=18) {
+//			System.out.println("eligible");
+//		}else {
+//			System.out.println("not a eligible");
+//		}
+//	}
+//}
+//
+
+//
+//public class VotingEligiblity {
+//	public static void main ( String[] args) {
+//		int age=20;
+//		
+//		if(age>=18) {
+//			System.out.println("u are eligible");
+//		}else {
+//			System.out.println("u are not eligible");
+//		}
+//	}
+//}
+
+
+//public class VotingEligiblity{
+//	public static void main(String[] args) {
+//		int age=25;
+//				if(age>=21) {
+//					System.out.println("u r eliglible");
+//				}else{
+//					System.out.println("u r not eligible");
+//				}
+//	}
+//}
