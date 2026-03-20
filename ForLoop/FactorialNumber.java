ca6becd78bee7fb925a31d2d847daeca1fc79936
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-21 00:05:41 +0530

    added Factorial Number, code

---
+++ ForLoop/FactorialNumber.java
@@ -0,0 +1,20 @@
+package ForLoop;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int n = sc.nextInt();
+
+        int fact = 1;
+
+        for(int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial = " + fact);
+    }
+}
