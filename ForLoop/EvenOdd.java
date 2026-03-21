cb3ded4d051ca3b140dd0f08c54df5c006e568fd
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-21 21:42:28 +0530

    Added EvenOdd ,code

---
+++ ForLoop/EvenOdd.java
@@ -0,0 +1,15 @@
+package ForLoop;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 10; i++) {
+            if(i % 2 == 0) {
+                System.out.println(i + " is Even");
+            } else {
+                System.out.println(i + " is Odd");
+            }
+        }
+    }
+}
