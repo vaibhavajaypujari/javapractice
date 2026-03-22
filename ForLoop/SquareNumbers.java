f27cf38d4c27a050315f8a84cd02c148b45ef8fc
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-22 22:01:02 +0530

    added Square of Numbers, code

---
+++ ForLoop/SquareNumbers.java
@@ -0,0 +1,11 @@
+package ForLoop;
+
+
+	public class SquareNumbers {
+	    public static void main(String[] args) {
+
+	        for(int i = 1; i <= 5; i++) {
+	            System.out.println("Square of " + i + " = " + (i * i));
+	        }
+	}
+}
