61795244f5510ce23259f07dfd08fb8bcd895ce0
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-23 19:11:49 +0530

    added multiples of five,code

---
+++ ForLoop/MultiplesOfFive.java
@@ -0,0 +1,9 @@
+package ForLoop;
+public class MultiplesOfFive {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(5 * i);
+        }
+    }
+}
