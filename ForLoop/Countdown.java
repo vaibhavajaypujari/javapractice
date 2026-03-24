395f996afd096165b0b8b3589b971de318a03e07
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-24 21:50:46 +0530

    added count down ,code

---
+++ ForLoop/Countdown.java
@@ -0,0 +1,11 @@
+package ForLoop;
+public class Countdown {
+    public static void main(String[] args) {
+
+        for(int i = 5; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Start!");
+    }
+}
