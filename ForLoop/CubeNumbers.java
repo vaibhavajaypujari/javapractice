c979a762369f3c19e4348d7594b71baa99ab3453
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-25 22:18:06 +0530

    added a Cube Of Numbers , code

---
+++ ForLoop/CubeNumbers.java
@@ -0,0 +1,9 @@
+package ForLoop;
+
+public class CubeNumbers {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("Cube of " + i + " = " + (i * i * i));
+        }
+    }
+}
