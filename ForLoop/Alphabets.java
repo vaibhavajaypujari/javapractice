da1c71de98ce9af74092e6ba359436afcc86462a
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-25 22:13:51 +0530

    added Alphabets A to Z & a to z ,code

---
+++ ForLoop/Alphabets.java
@@ -0,0 +1,9 @@
+package ForLoop;
+
+public class Alphabets {
+    public static void main(String[] args) {
+        for(char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.println(ch);
+        }
+    }
+}
