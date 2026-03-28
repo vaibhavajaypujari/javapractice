32323549d953a96a18cb8b9154ef7c08b2047aa3
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-28 21:30:09 +0530

    added a example of while loop ,code

---
+++ whileLoop/Example.java
@@ -0,0 +1,14 @@
+package whileLoop;
+
+public class Example {
+	public static void main(String[] args) {
+		int i =1;
+		
+		while(i<=5) {
+			System.err.println("Number : " + i);
+			i++;
+		}
+		
+	}
+
+}
