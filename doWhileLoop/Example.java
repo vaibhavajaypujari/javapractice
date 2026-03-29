0042c97eee6ef70cd2b42e25e8e6316b18059ec7
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-29 22:11:37 +0530

    added a example of do while,code

---
+++ doWhileLoop/Example.java
@@ -0,0 +1,13 @@
+package doWhileLoop;
+
+public class Example {
+	public static void main(String[] args) {
+		int i=1;
+		
+		do {
+			System.out.println("Number : "+i);
+			i++;
+		}while(i<=5);
+	}
+
+}
