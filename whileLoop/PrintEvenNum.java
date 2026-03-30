fb7e38a2dd8fcb00afb9d7d9a6999f6931c41a64
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-03-30 21:42:45 +0530

    added a Print even Number,Code

---
+++ whileLoop/PrintEvenNum.java
@@ -0,0 +1,38 @@
+package whileLoop;
+
+public class PrintEvenNum {
+	public static void main(String[] args) {
+		int i = 2;
+		
+		while(i<=20) {
+			System.out.println(i);
+			i=i+2;
+		}
+	}
+}
+
+
+
+
+//example 
+
+
+//public static void main(String[] args) {
+//	int i=2;
+//	while(i<=100){
+//	System.out.println(i);
+//	i=i+2;
+//	}
+//}
+//}
+
+
+
+//example
+//	public static void main(String[] args) {
+//		int i=2;
+//		while(i<=5000) {
+//			System.out.println(i);
+//			i=i+2;
+//		}
+//	}}
