9a6a816afffb3cb157b181f7d82bc99331c7a7a4
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-01 21:16:23 +0530

    added a Reverse a Number ,code

---
+++ doWhileLoop/ReverseNum.java
@@ -0,0 +1,35 @@
+package doWhileLoop;
+
+//public class ReverseNum {
+//	public static void main(String[] args) {
+//		int i=10;
+//		do {
+//			System.out.println(i);
+//			i--;
+//		}while(i>=1);
+//	}
+//
+//}
+
+//example
+//public class ReverseNum{
+//	public static void main(String[] args) {
+//		int i=500;
+//		do {
+//			System.out.println(i);
+//			i--;
+//		}while(i>=1);
+//	}
+//}
+
+//example
+
+public class ReverseNum{
+	public static void main(String[] args) {
+		int i=2000;
+		do {
+			System.out.println(i);
+			i--;
+		}while(i>=1);
+	}
+}
