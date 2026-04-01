3fadd80d76114b677dd654ffc6c0cff6dfed3f88
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-01 22:36:58 +0530

    added a Square of a Numbers,code

---
+++ whileLoop/SquareOfNum.java
@@ -0,0 +1,37 @@
+package whileLoop;
+
+//public class SquareOfNum {
+//public static void main(String[] args) {
+//	int i=1;
+//	
+//	while(i<=5) {
+//		System.out.println(i+(i*i));
+//		i++;
+//		}
+//	}
+//}
+
+
+//example
+
+//public class SquareOfNum{
+//	public static void main(String[] args) {
+//		int i=1;
+//		
+//		while(i<=50){
+//		System.out.println("Square of "+ i + " = " + (i * i));
+//		i++;
+//	}}
+//}
+
+//example
+
+public class SquareOfNum{
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=100) {
+			System.out.println("Square of " + i + " = " + (i * i));
+			i++;
+		}
+	}
+}
