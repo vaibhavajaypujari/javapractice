6d910428b6542b1d9a06207b27fcded971b7e332
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-01 22:38:20 +0530

    added a Divisible of Numbers,code

---
+++ doWhileLoop/DivOfNum.java
@@ -0,0 +1,46 @@
+package doWhileLoop;
+
+public class DivOfNum {
+public static void main(String[] args) {
+	int i = 1;
+	
+	do {
+		if(i%5==0) {
+			System.out.println(i);
+		}
+		i++;
+	}while(i<= 25);
+}
+}
+
+//example
+
+//public class DivOfNum{
+//	public static void main(String[] args) {
+//		int i = 1;
+//		
+//		do {
+//			if(i%8==0) {
+//				System.out.println(i);
+//			}
+//			i++;
+//		}while(i<=50);
+//	}
+//}
+
+
+//example
+
+//public class DivOfNum{
+//	public static void main(String[] args) {
+//		int i = 1;
+//		
+//		do {
+//			if(i%10==0) {
+//				System.out.println(i);
+//			}
+//			i++;
+//		}
+//		while(i<=100);
+//	}
+//}
