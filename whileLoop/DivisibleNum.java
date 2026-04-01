578f3538c17f47cd12a107ff7ed4f618c2b88d4d
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-01 22:35:41 +0530

    added a Divisible by  Num ,code

---
+++ whileLoop/DivisibleNum.java
@@ -0,0 +1,45 @@
+package whileLoop;
+
+public class DivisibleNum {
+	public static void main(String[] args) {
+		int i=1;
+		
+		while(i<=150) {
+			if(i% 5 == 0 ) {
+				System.out.println(i);
+			}
+			i++;
+		}
+	}
+}
+
+
+//example
+//
+//public class DivisibleNum{
+//public static void main(String[] args) {
+//	int i=1;
+//	
+//	while(i<=500) {
+//		if(i%10==0) {
+//			System.out.println(i);
+//		}
+//		i++;
+//	}
+//}
+//}
+
+//example
+
+//public class DivisibleNum{
+//	public static void main(String[] args) {
+//		int i=1;
+//		
+//		while(i<=400) {
+//			if(i%8==0) {
+//				System.out.println(i);
+//			}
+//			i++;
+//		}
+//	}
+//}
