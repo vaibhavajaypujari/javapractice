62af90f3aa83507357e8e8feaf4b42fd48f76efa
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-01 21:15:03 +0530

    added a Odd Number,Code

---
+++ doWhileLoop/OddNumber.java
@@ -0,0 +1,36 @@
+package doWhileLoop;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println(i);
+			
+			i=i+2;
+			
+		} while(i<=15);
+	}
+}
+
+//example
+//public class OddNumber{
+//	public static void main(String[] args) {
+//		int i =1;
+//		do {
+//			System.out.println(i);
+//			i=i+2;
+//		}while(i<=200);
+//	}
+//}
+
+//example 
+//public class OddNumber{
+//	public static void main(String[] args) {
+//	int i=1;
+//	do {
+//		System.out.println(i);
+//		i=i+2;
+//		}while(i<=1000);
+//}
+//}
