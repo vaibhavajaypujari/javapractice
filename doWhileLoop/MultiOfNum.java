5845de8096cc75692a3e9f476d1e78d47aa0387a
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-01 22:42:16 +0530

    added a Multiple of Numbers ,code

---
+++ doWhileLoop/MultiOfNum.java
@@ -0,0 +1,40 @@
+package doWhileLoop;
+
+public class MultiOfNum {
+	public static void main(String[] args) {
+		int i=1;
+		
+		do {
+			System.out.println("4 x " + i + " = " + ( 4 * i));
+			i++;
+		}while(i <= 10);
+	}
+
+}
+
+
+//example
+
+//public class MultiOfNum{
+//	public static void main(String[] args) {
+//		int i=1;
+//		
+//		do {
+//			System.out.println("5 x "+ i + " = " + (5 * i));
+//			i++;
+//		} while (i<=10);
+//	}
+//}
+
+//example 
+
+//public class MultiOfNum{
+//	public static void main(String[] args) {
+//		int  i = 1;
+//		
+//		do {
+//			System.out.println("15 x " + i + " = "+ (15 * i));
+//			i++;
+//		}while(i<=100);
+//	}
+//}
