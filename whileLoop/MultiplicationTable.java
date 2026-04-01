01cd56838ec8c005e4316b3a013f043f6bd5dc32
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-01 21:12:33 +0530

    added a Multiplication Table , code

---
+++ whileLoop/MultiplicationTable.java
@@ -0,0 +1,13 @@
+package whileLoop;
+
+public class MultiplicationTable {
+	public static void main(String[] args) {
+		int i=1;
+		
+		while(i<=10) {
+			System.out.println("5 x "+ i + " = " +(5*i));
+			i++;
+		}
+	}
+
+}
