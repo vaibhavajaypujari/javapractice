b2f6171406a83d821db1c6348ea59f25e8839043
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-01 21:13:44 +0530

    added a Reverse Counting , Code

---
+++ whileLoop/ReverseCounting.java
@@ -0,0 +1,40 @@
+package whileLoop;
+
+public class ReverseCounting {
+
+	public static void main(String[] args) {
+		int i=10;
+		while(i>=1) {
+			System.out.println(i);
+			i--;
+		}
+	}
+}
+
+
+
+
+//Example
+//public class ReverseCounting{
+//	public static void main(String[] args) {
+//		int i = 100;
+//		while(i>=1) {
+//			System.out.println(i);
+//			i--;
+//		}
+//	}
+//}
+
+
+//Example
+
+//public class ReverseCounting{
+//	public static void main(String[] args) {
+//		int i=5000;
+//		
+//		while(i>=1) {
+//			System.out.println(i);
+//			i--;
+//		}
+//	}
+//}
