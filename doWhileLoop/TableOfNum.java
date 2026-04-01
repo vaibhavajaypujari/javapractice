eb21960e1c5933fe7ec3f6a8dd5bf68976cd8617
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-01 21:20:13 +0530

    added a Table of Numbers,code

---
+++ doWhileLoop/TableOfNum.java
@@ -0,0 +1,39 @@
+package doWhileLoop;
+
+//public class TableOfNum {
+//	public static void main(String[] args) {
+//		int i=1;
+//		
+//		do {
+//			System.out.println("3 x "+i+" = " + (3 * i));
+//			i++;
+//		}while(i<=10);
+//	}
+//}
+
+//example
+
+//public class TableOfNum{
+//	public static void main(String[] args) {
+//		int i=1;
+//		do {
+//			System.out.println(i*5);
+//			i++;
+//			
+//		}while(i<=10);
+//	}
+//}
+
+
+
+//example
+
+public class TableOfNum{
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println(i*50);
+			i++;
+		}while(i<=10);
+	}
+}
