c7b56a5b7d715a270709a99832ea442ab742cf8c
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-01 22:34:31 +0530

    Added a cubes of number, code

---
+++ whileLoop/CubesOfNum.java
@@ -0,0 +1,41 @@
+package whileLoop;
+
+public class CubesOfNum {
+
+	public static void main(String[] args) {
+		int i = 1;
+		
+		while(i<=5) {
+			System.out.println("cubes of "+ i + " = " + ( i * i * i));
+			i++;
+		}
+	}
+}
+
+
+//example
+
+//public class CubesOfNum{
+//	public static void main(String[] args) {
+//	
+//		int i =1;
+//		
+//		while(i<=10) {
+//			System.out.println(i+(i*i*i));
+//			i++;
+//		}
+// }
+//}
+
+//example
+
+//public class CubesOfNum{
+//	public static void main(String[] args) {
+//		int i = 1;
+//		
+//		while(i<=7) {
+//			System.out.println("Cube of "+ i +" = "+ (i*i*i));
+//			i++;
+//		}
+//	}
+//}
