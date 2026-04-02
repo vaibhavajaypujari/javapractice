31cc8c9f7886835d41791a8b7e3c92375fdede6b
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-02 21:24:17 +0530

    Added a Print Numbers Scanner , code

---
+++ ForLoop/PrintNumbersScanner.java
@@ -0,0 +1,18 @@
+package ForLoop;
+
+import java.util.Scanner;
+
+public class PrintNumbersScanner {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println(" Enter` a number : ");
+		int n= sc.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			System.out.println(i);
+		}
+	}
+}
