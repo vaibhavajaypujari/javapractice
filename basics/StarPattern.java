202beed5ef7f0c1967fafb737a621bae79b39556
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-07 22:24:33 +0530

    added a Star Pattern ,Code

---
+++ basics/StarPattern.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class StarPattern {
+	public static void main (String[] args) {
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+	}
+
+}
