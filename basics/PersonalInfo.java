78e1006bbcdbf2d9116fbccbf36bee95c6c98bf8
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-08 23:32:34 +0530

    added a Personal info,code

---
+++ basics/PersonalInfo.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class PersonalInfo {
+	public static void main(String[] args) {
+		System.out.println("Name: Vaibhav");
+		System.out.println("Collage: PVPIT,Sangli");
+		System.out.println("Dept: E&TC");
+		System.out.println("Dura: Four Year");
+	}
+
+}
