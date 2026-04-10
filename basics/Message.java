295ca5f1173e57f1e20018f8a4d01bb72f067511
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-10 21:45:37 +0530

    added a (message show on o/p) ,code

---
+++ basics/Message.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class Message {
+    public static void main(String[] args) {
+
+        System.out.println("Java Programming Language");
+        System.out.println("Platform Independent");
+        System.out.println("Object Oriented Language");
+
+    }
+}
