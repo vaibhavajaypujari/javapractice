3adcecb729887ff455df1d9c5323d20e728cd716
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-11 21:58:40 +0530

    added a basic Table Format,code

---
+++ basics/TableFormat.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class TableFormat {
+
+    public static void main(String[] args) {
+
+        System.out.println("Name\tAge\tCity\tCompany");
+        System.out.println("Vaibhav\t23\tPune\tTCS");
+        System.out.println("Omkar\t22\tMumbai\tWipro");
+        System.out.println("Aman\t24\tDelhi\tL&T");
+
+    }
+}
