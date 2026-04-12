4f6c43013d5847b3f7ed3a04bb699dc53a397758
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-12 21:48:17 +0530

    added a simple design,code

---
+++ basics/SimpleDesign.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class SimpleDesign {
+    public static void main(String[] args) {
+
+        System.out.println("###########");
+        System.out.println("#         #");
+        System.out.println("#  JAVA   #");
+        System.out.println("#         #");
+        System.out.println("###########");
+
+    }
+}
