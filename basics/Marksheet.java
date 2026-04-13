958f4324d93dac660c0e3a1a21c6d7effc878f72
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-13 21:35:55 +0530

    added a Marksheet , code

---
+++ basics/Marksheet.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class Marksheet {
+	    public static void main(String[] args) {
+
+	        System.out.println("----- Student Marksheet -----");
+	        System.out.println("Name   : Vaibhav");
+	        System.out.println("Course : CDAC");
+	        System.out.println("Java   : 85");
+	        System.out.println("DBMS   : 80");
+	        System.out.println("DSA    : 78");
+	        System.out.println("-----------------------------");
+
+	    }
+	}
+
+
