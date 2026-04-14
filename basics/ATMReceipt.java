76610c3bb04d9e42bba8cea5af7e1665bcc1d60a
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-14 21:02:07 +0530

    added a ATM Receipt, code

---
+++ basics/ATMReceipt.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class ATMReceipt {
+
+    public static void main(String[] args) {
+
+        System.out.println("------ ATM RECEIPT ------");
+        System.out.println("Bank Name : ABC Bank");
+        System.out.println("Account   : XXXX1234");
+        System.out.println("Withdrawn : 2000");
+        System.out.println("Balance   : 8000");
+        System.out.println("-------------------------");
+        System.out.println("Thank You Visit Again");
+
+    }
+}
