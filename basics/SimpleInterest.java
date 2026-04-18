ed8fe23c4c6af8ec70b150292357a0de2cd6704e
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-18 21:30:51 +0530

    added a simple interest ,code

---
+++ basics/SimpleInterest.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class SimpleInterest {
+	public static void main(String[] args) {
+		
+		int p=7;
+		int r=5;
+		int t=2;
+		
+		int si=(p*r*t)/100;
+		System.out.println("Simple Interest: "+ si);
+	}
+
+}
