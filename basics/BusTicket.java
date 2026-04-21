0fafb95e2a8b1927e4d5d422f5f852a17794ae01
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-21 19:21:29 +0530

    added a bus ticket format,code

---
+++ basics/BusTicket.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class BusTicket {
+	public static void main(String[] args) {
+
+        System.out.println("-------- BUS TICKET --------");
+        System.out.println("Passenger Name : Vaibhav");
+        System.out.println("From           : Pune");
+        System.out.println("To             : Mumbai");
+        System.out.println("Seat No        : 12");
+        System.out.println("Fare           : 350");
+        System.out.println("----------------------------");
+        System.out.println("Happy Journey!");
+
+    }
+
+}
