f2db45643d9f5329f0ee36ac4208ceeb05a15c6e
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-22 23:30:03 +0530

    added a identy card format ,code

---
+++ basics/IDCard.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class IDCard {
+
+
+	    public static void main(String[] args) {
+
+	        System.out.println("------- STUDENT ID CARD -------");
+	        System.out.println("Name   : Vaibhav Pujari");
+	        System.out.println("Course : MCA");
+	        System.out.println("Roll No: 101");
+	        System.out.println("College: ABC College");
+	        System.out.println("-------------------------------");
+
+	    }
+	}
+	
+
