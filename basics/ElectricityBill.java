7d6a3af620deb82e0323c1d08f601bf8c8b031f2
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-04-22 23:34:23 +0530

    added a eletricity bill format ,code

---
+++ basics/ElectricityBill.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class ElectricityBill {
+    public static void main(String[] args) {
+
+        System.out.println("------ ELECTRICITY BILL ------");
+        System.out.println("Consumer Name : Vaibhav");
+        System.out.println("Meter No      : 458796");
+        System.out.println("Units Used    : 120");
+        System.out.println("Amount        : 950 Rs");
+        System.out.println("------------------------------");
+        System.out.println("Pay Before Due Date");
+
+    }
+
+}
