27c3e66ae3c00e0a7c5129a322cf4e45b6dd0a79
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-05-04 22:25:52 +0530

    added a Product Digit While, code

---
+++ whileLoop/ProductDigitsWhile.java
@@ -0,0 +1,27 @@
+package whileLoop;
+
+import java.util.Scanner;
+
+public class ProductDigitsWhile {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		int num = sc.nextInt();
+
+		int product = 1;
+
+		while (num != 0) {
+			int digit = num % 10;
+			product = product * digit;
+			num = num / 10;
+		}
+
+		System.out.println("Product of digits = " + product);
+	}
+
+}
+
+// i/p : 234
