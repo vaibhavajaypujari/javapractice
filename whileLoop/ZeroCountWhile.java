9908f0d82c520c4a1dc0d437cf9161cf54b1e7aa
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-05-06 20:09:58 +0530

    added a count of zeros,code

---
+++ whileLoop/ZeroCountWhile.java
@@ -0,0 +1,27 @@
+package whileLoop;
+
+import java.util.Scanner;
+
+public class ZeroCountWhile {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		int num = sc.nextInt();
+
+		int count = 0;
+
+		while (num != 0) {
+			int digit = num % 10;
+
+			if (digit == 0) {
+				count++;
+			}
+
+			num = num / 10;
+		}
+
+		System.out.println("Total zeros = " + count);
+	}
+}
