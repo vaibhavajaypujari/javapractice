6026e40271d09632b50b899cd55bcd79b94fd1b3
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-05-09 19:32:02 +0530

    added a odd digits sum ,code

---
+++ doWhileLoop/OddDigitSumDoWhile.java
@@ -0,0 +1,28 @@
+package doWhileLoop;
+
+import java.util.Scanner;
+
+public class OddDigitSumDoWhile {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		int num = sc.nextInt();
+
+		int sum = 0;
+
+		do {
+			int digit = num % 10;
+
+			if (digit % 2 != 0) {
+				sum = sum + digit;
+			}
+
+			num = num / 10;
+
+		} while (num != 0);
+
+		System.out.println("Sum of odd digits = " + sum);
+	}
+}
