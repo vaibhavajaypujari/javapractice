2d27cf36bd06fe3b18c8d092acc8aa83f27e88d0
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-05-10 19:48:57 +0530

    addded a simple welcome message,code

---
+++ basics/WelcomeMessage.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class WelcomeMessage {
+
+	public static void main(String[] args) {
+		System.out.println("************************");
+		System.out.println("   Welcome to Java");
+		System.out.println("   Practice Program");
+		System.out.println("************************");
+	}
+}
