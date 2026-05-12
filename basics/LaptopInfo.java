069431919a364ba6f03fbf68b4a487153664b5ca
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-05-12 19:49:50 +0530

    added a laptop info simple code ,java

---
+++ basics/LaptopInfo.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class LaptopInfo {
+
+	public static void main(String[] args) {
+		System.out.println("Laptop Brand : ASUS");
+		System.out.println("Model        : TUF Gaming A15");
+		System.out.println("Processor    : Ryzen");
+		System.out.println("Purpose      : Coding and Learning Java");
+	}
+}
