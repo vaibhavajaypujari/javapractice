ebbbc5011775a8b43a5b83b2c9b7e260659ae5a0
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-05-12 19:55:27 +0530

    added a count digits ,code

---
+++ whileLoop/CountDigits.java
@@ -0,0 +1,24 @@
+package whileLoop;
+
+import java.util.Scanner;
+
+public class CountDigits {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num = sc.nextInt();
+
+		int count = 0;
+
+		while (num != 0) {
+			num = num / 10;
+			count++;
+		}
+
+		System.out.println("Total digits: " + count);
+
+		sc.close();
+	}
+}
