fdaa5c87fdb9c716c075f6fb39444d1eba34771e
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-05-16 23:54:51 +0530

    Added Java program for Anagram String Check

---
+++ Imp/AnagramCheck.java
@@ -0,0 +1,24 @@
+package Imp;
+
+import java.util.Arrays;
+
+public class AnagramCheck {
+
+	public static void main(String[] args) {
+
+		String str1 = "listen";
+		String str2 = "silent";
+
+		char[] arr1 = str1.toCharArray();
+		char[] arr2 = str2.toCharArray();
+
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+
+		if (Arrays.equals(arr1, arr2)) {
+			System.out.println("Strings are Anagrams");
+		} else {
+			System.out.println("Strings are not Anagrams");
+		}
+	}
+}
