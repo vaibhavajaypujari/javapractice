11900dc3b9c49100bdcc130c6e4b730d24a7dbc8
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-05-19 22:09:28 +0530

    added a reverse array ,code

---
+++ Imp/ReverseArray.java
@@ -0,0 +1,15 @@
+package Imp;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 10, 20, 30, 40, 50 };
+
+		System.out.println("Reversed Array:");
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
