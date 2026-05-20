bbf6fd579c10cd10cca93d327254f749f2374381
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-05-20 20:31:40 +0530

    added a vowel consonant ,code

---
+++ Imp/VowelConsonant.java
@@ -0,0 +1,18 @@
+package Imp;
+
+public class VowelConsonant {
+
+	public static void main(String[] args) {
+
+		char ch = 'a';
+
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+				|| ch == 'O' || ch == 'U') {
+
+			System.out.println(ch + " is a Vowel");
+
+		} else {
+			System.out.println(ch + " is a Consonant");
+		}
+	}
+}
