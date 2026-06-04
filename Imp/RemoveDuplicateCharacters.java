bb54bb65180308ac66bf3cfebb850f630d8eddf2
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-06-04 17:11:04 +0530

    added a Remove DuplicatesCharacters,java

---
+++ Imp/RemoveDuplicateCharacters.java
@@ -0,0 +1,22 @@
+package Imp;
+
+public class RemoveDuplicateCharacters {
+
+	public static void main(String[] args) {
+
+		String str = "programming";
+		String result = "";
+
+		for (int i = 0; i < str.length(); i++) {
+
+			char ch = str.charAt(i);
+
+			if (result.indexOf(ch) == -1) {
+				result += ch;
+			}
+		}
+
+		System.out.println("Original String: " + str);
+		System.out.println("After Removing Duplicates: " + result);
+	}
+}
