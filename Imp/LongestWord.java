2c4cd3411ea197aaea550da978b6f0f50561d037
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-06-13 22:33:09 +0530

    added LongestWord code

---
+++ Imp/LongestWord.java
@@ -0,0 +1,23 @@
+package Imp;
+
+public class LongestWord {
+
+	public static void main(String[] args) {
+
+		String sentence = "Java is a powerful programming language";
+
+		String[] words = sentence.split(" ");
+
+		String longest = "";
+
+		for (String word : words) {
+
+			if (word.length() > longest.length()) {
+				longest = word;
+			}
+		}
+
+		System.out.println("Longest Word: " + longest);
+		System.out.println("Length: " + longest.length());
+	}
+}
