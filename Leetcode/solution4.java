2ddad707de63d4b72a938af8d546228385d96a80
vaibhavajaypujari <vaibhavpujari2020@gmail.com>
2026-07-07 23:48:07 +0530

    Concatenate Non-Zero Digits and Multiply by Sum I

---
+++ Leetcode/solution4.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class solution4 {
+
+	public long sumAndMultiply(int n) {
+		long x = 0, s = 0;
+		for (char c : String.valueOf(n).toCharArray())
+			if (c != '0') {
+				x = x * 10 + c - '0';
+				s += c - '0';
+			}
+		return x * s;
+	}
+}
